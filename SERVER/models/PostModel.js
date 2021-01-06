01bfb1294d15370c55978316d8b6a9fa3d55843e
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-01-06 15:34:02 +0800

    create models

---
+++ SERVER/models/PostModel.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose')
+
+const postSchema = new Schema({
+    body: String,
+    username: String,
+    createdAt: String,
+    comments: [
+        {
+            body: String,
+            username: String,
+            createdAt: String
+        }
+    ],
+    likes: [
+        {
+            username: String,
+            createdAt: String
+        }
+    ],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    }
+})
+
+module.exports = model('posts', postSchema)
