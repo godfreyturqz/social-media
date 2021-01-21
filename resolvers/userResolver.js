b6b883a17e67f164b222de112589ceb6eee9ed73
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-01-21 19:41:58 +0800

    Create user login resolver

--- resolvers/userResolver.js
+++ resolvers/userResolver.js
@@ -24,6 +24,28 @@ const userResolver = {
             createdAt: newUser.createdAt,
             token
         }
+    },
+    login: async (parent, args) => {
+        const { loginInput: { email, password } } = args
+
+        const errors = userInputValidator(email, password, null)
+        if(Object.keys(errors).length >= 1) throw new UserInputError('User input validation error', { errors })
+
+        const userData = await UserModel.findOne({email})
+        if(userData === null) throw new UserInputError('Account does not exists')
+
+        const isMatch = await comparePassword(password, userData.password)
+        if(isMatch){
+            const token = createJWT(userData._id)
+            return {
+                id: userData._id,
+                email: userData.email,
+                createdAt: userData.createdAt,
+                token
+            }
+        } else {
+            throw Error('Some of your information isn\'t correct. Please try again') 
+        }
     }
 }
 
