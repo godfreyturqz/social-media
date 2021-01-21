585c482e4e57498d20e1ec7adc9f30377c216501
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-01-21 19:40:30 +0800

    Create utils for hashing and comparing passwords

---
+++ utils/password.js
@@ -0,0 +1,16 @@
+const bcrypt = require('bcrypt')
+
+module.exports.hashPassword = async (password) => {
+    
+    const salt = await bcrypt.genSalt()
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    return hashedPassword
+}
+
+module.exports.comparePassword = async (loginPassword, dbPassword) => {
+
+    const isMatch = await bcrypt.compare(loginPassword, dbPassword)
+
+    return isMatch
+}
