d644c94424339458043d31f0e09913190d7e3016
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-01-23 22:09:55 +0800

    create and delete post mutation

--- resolvers/postResolver.js
+++ resolvers/postResolver.js
@@ -7,6 +7,31 @@ const postResolver = {
             return posts
         } catch (error) {
             throw new Error(error)
+    Mutation: {
+        createPost: async (parent, args, context) => {
+            const user = checkAuth(context)
+            const { body } = args
+            const newPost = await PostModel.create({
+                body,
+                user: user.id,
+                createdAt: new Date().toString()
+            })
+            return newPost
+        },
+        deletePost: async (parent, args, context) => {
+            const user = checkAuth(context)
+            const { postId } = args
+            try {
+                const post = await PostModel.findById(postId).populate('users').then((res) => console.log(res))
+                if(user.email === post.email){
+                    const deletedPost = await PostModel.findByIdAndRemove(postId)
+                    return deletedPost
+                } else {
+                    throw new AuthenticationError('Action not allowed')
+                }
+            } catch (error) {
+                throw new Error(error)
+            }
         }
     }
 }
