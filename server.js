dac5aa2c80b2e36f44e1335622a5d279135d44b8
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-01-23 21:13:47 +0800

    remove mongoose deprecation warning

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.listen({port: PORT}, () => {
 // ------------------------------
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify: false
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(error => console.log(error))
