ef8ec14ee5fd82e56884fad40fef33368d86895f
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-01-23 22:04:30 +0800

    Update userResolver.js

--- resolvers/userResolver.js
+++ resolvers/userResolver.js
@@ -6,55 +6,54 @@ const { hashPassword, comparePassword } = require('../utils/password')
 const { createJWT } = require('../utils/createJWT')
 
 
-const userResolver = {
-    register: async (parent, args) => {
-        const { registerInput: { email, password, confirmPassword } } = args
+module.exports = {
+    Mutation: {
+        register: async (parent, args) => {
+            const { registerInput: { email, password, confirmPassword } } = args
+    
+            const errors = userInputValidator(email, password, confirmPassword)
+            if(Object.keys(errors).length >= 1) throw new UserInputError('User input validation error', { errors })
+    
+            const isEmailExists = await UserModel.findOne({email})
+            if(isEmailExists) throw new UserInputError('Account already exists')
+    
+            const hashedPassword = await hashPassword(password)
+    
+            const newUser = await UserModel.create({
+                email,
+                password: hashedPassword,
+                createdAt: new Date().toString()
+            })
+    
+            const token = createJWT(newUser._id)
 
-        const errors = userInputValidator(email, password, confirmPassword)
-        if(Object.keys(errors).length >= 1) throw new UserInputError('User input validation error', { errors })
+            return {
+                id: newUser._id,
+                email: newUser.email,
+                createdAt: newUser.createdAt,
+                token
+            }
+        },
+        login: async (parent, args) => {
+            const { loginInput: { email, password } } = args
+    
+            const errors = userInputValidator(email, password, null)
+            if(Object.keys(errors).length >= 1) throw new UserInputError('User input validation error', { errors })
+    
+            const userData = await UserModel.findOne({email})
+            if(userData === null) throw new UserInputError('Account does not exists')
+    
+            const isMatch = await comparePassword(password, userData.password)
+            if(!isMatch) throw Error('Some of your information isn\'t correct. Please try again')
 
-        const isEmailExists = await UserModel.findOne({email})
-        if(isEmailExists) throw new UserInputError('Account already exists')
-
-        const hashedPassword = await hashPassword(password)
-
-        const newUser = await UserModel.create({
-            email,
-            password: hashedPassword,
-            createdAt: new Date().toString()
-        })
-
-        const token = createJWT(newUser._id)
-
-        return {
-            id: newUser._id,
-            email: newUser.email,
-            createdAt: newUser.createdAt,
-            token
-        }
-    },
-    login: async (parent, args) => {
-        const { loginInput: { email, password } } = args
-
-        const errors = userInputValidator(email, password, null)
-        if(Object.keys(errors).length >= 1) throw new UserInputError('User input validation error', { errors })
-
-        const userData = await UserModel.findOne({email})
-        if(userData === null) throw new UserInputError('Account does not exists')
-
-        const isMatch = await comparePassword(password, userData.password)
-        if(isMatch){
             const token = createJWT(userData._id)
+
             return {
                 id: userData._id,
                 email: userData.email,
                 createdAt: userData.createdAt,
                 token
             }
-        } else {
-            throw Error('Some of your information isn\'t correct. Please try again') 
         }
     }
 }
-
-module.exports = userResolver
