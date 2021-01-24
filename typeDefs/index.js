80ce5697d54c58c7138f18bd18e344cd77a19849
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-01-24 17:29:36 +0800

    remove commas for simplicity

--- typeDefs/index.js
+++ typeDefs/index.js
@@ -26,15 +26,15 @@ const typeDefs = gql`
         username: String
     }
     type User {
-        id: ID,
-        email: String,
-        createdAt: String,
+        id: ID
+        email: String
+        createdAt: String
         token: String
     }
     type Post {
-        id: ID,
-        body: String,
-        createdAt: String,
+        id: ID
+        body: String
+        createdAt: String
         username: String
         comments: [Comment]
         likes: [Like]
