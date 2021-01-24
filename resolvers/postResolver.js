0e8a97ddebec48cf6c0fd394128cf96525621d55
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-01-24 17:47:09 +0800

    add whitespaces

--- resolvers/postResolver.js
+++ resolvers/postResolver.js
@@ -27,24 +27,27 @@ module.exports = {
         createPost: async (parent, args, context) => {
             const user = checkAuth(context)
             const { body } = args
+
             const newPost = await PostModel.create({
                 body,
                 user: user.id,
                 createdAt: new Date().toString()
             })
+
             return newPost
         },
         deletePost: async (parent, args, context) => {
             const user = checkAuth(context)
             const { postId } = args
+
             try {
-                const post = await PostModel.findById(postId).populate('users').then((res) => console.log(res))
-                if(user.email === post.email){
-                    const deletedPost = await PostModel.findByIdAndRemove(postId)
-                    return deletedPost
-                } else {
-                    throw new AuthenticationError('Action not allowed')
-                }
+                const post = await PostModel.findById(postId)
+                if(user.email !== post.email) throw new AuthenticationError('Action not allowed')
+                
+                const deletedPost = await PostModel.findByIdAndRemove(postId)
+
+                return deletedPost
+
             } catch (error) {
                 throw new Error(error)
             }
