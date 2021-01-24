31b9e8e798cc65ab267644177b11363ae776a4f3
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-01-24 17:27:44 +0800

    add createComment resolver

--- resolvers/commentResolver.js
+++ resolvers/commentResolver.js
@@ -4,5 +4,23 @@ const { checkAuth } = require('../utils/checkAuth')
 
 module.exports = {
     Mutation: {
+        createComment: async (parent, args, context) => {
+            const { postId, body } = args
+            const user = checkAuth(context)
+
+            if(body.trim() === '') throw new UserInputError('Empty comment')
+
+            const post = await PostModel.findById(postId)
+            if(!post) throw new UserInputError('Post not found')
+            
+            post.comments.unshift({
+                body,
+                username: user.username,
+                createdAt: new Date().toString()
+            })
+            await post.save()
+
+            return post
+        },
     }
 }
