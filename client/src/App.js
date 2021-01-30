333001aa5db1820d9a6c846a9814988937a733fa
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-01-30 11:30:43 +0800

    apollo client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css'
+import { ApolloProvider } from '@apollo/client'
+import { client } from './ApolloProvider'
 
 function App() {
   return (
+    <ApolloProvider client={client}>
       <div>
         Apollo Client
       </div>
+    </ApolloProvider>
   )
 }
 
