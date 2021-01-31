e9cbcc9ed2cf790abb90586b358bf74d4f8de120
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-01-31 12:26:45 +0800

    create ApolloClient config

--- client/src/apollo/config.js
+++ client/src/apollo/config.js
@@ -1,7 +1,11 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
 
 const LINK = createHttpLink({
+<<<<<<< Updated upstream
     uri: 'http://localhost:5000/graphql'
+=======
+    uri: "http://localhost:5000/graphql"
+>>>>>>> Stashed changes
 })
 
 export const client = new ApolloClient({
