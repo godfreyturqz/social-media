973a7397bb0c252b083e208ba2707350e29286af
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-02-20 15:22:23 +0800

    create Authenticated route reusable component

---
+++ client/src/utils/AuthRoute.js
@@ -0,0 +1,16 @@
+import { useContext } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { AuthContext } from '../context/authContext'
+
+const AuthRoute = ({component: Component, ...rest}) => {
+    const { user } = useContext(AuthContext)
+
+    return (
+        <Route
+            {...rest}
+            render = { props => user ? <Redirect to="/" /> : <Component {...props} /> }
+        />
+    )
+}
+
+export default AuthRoute
