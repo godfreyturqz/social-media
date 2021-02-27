b94511f53a90f2f85cc04288b95c19d6d87608f4
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-02-27 19:08:27 +0800

    Update Home page

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,23 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/client'
 import { client } from './apollo/config'
 import 'semantic-ui-css/semantic.min.css'
-// components
-import NavBar from './components/NavBar'
 // pages
 import Home from './pages/Home'
 import Register from './pages/Register'
-import Login from './pages/Login'
 // context
 import { AuthProvider } from './context/authContext'
 import AuthRoute from './utils/AuthRoute'
 
 const App = () => {
+
   return (
     <div className="ui container">
       <AuthProvider>
         <ApolloProvider client={client}>
           <BrowserRouter>
-            <NavBar/>
             <Switch>
-              <Route exact path="/" component={Home} />
-              <AuthRoute exact path="/register" component={Register} />
-              <AuthRoute exact path="/login" component={Login} />
+              <AuthRoute exact path="/" component={Home} />
+              <Route exact path="/register" component={Register} />
             </Switch>
           </BrowserRouter>
         </ApolloProvider>
